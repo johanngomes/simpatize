956d3812ed6a9f532295e53280f6a966d4c980cc
johanngomes <johanngomes@gmail.com>
2015-10-24 15:31:05 -0300

    Vagrant syncronizing the whole project folders.

--- vagrant/web-service/simpatize/urls.py
+++ vagrant/web-service/simpatize/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'simpatize.views.index'),
     url(r'^admin/', include(admin.site.urls)),
 ]
