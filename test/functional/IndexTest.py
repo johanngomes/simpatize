6077545bc064be9c034bc778406db48e1b27a6d9
johanngomes <johanngomes@gmail.com>
2015-10-27 14:42:14 -0300

    IndexTest.py refactored.

--- test/functional/IndexTest.py
+++ test/functional/IndexTest.py
@@ -5,12 +5,15 @@ from selenium import webdriver
 
 class IndexTest(unittest.TestCase):
 
-    def test_simpatize_on_page_title(self):
-        driver = webdriver.Firefox()
-        driver.get("http://localhost:8000/")
-        assert "Simpatize" in driver.title
-        driver.close()
+    def setUp(self):
+        self.driver = webdriver.Firefox()
 
+    def test_simpatize_on_page_title(self):
+        self.driver.get("http://localhost:8000/")
+        assert "Simpatize" in self.driver.title
+
+    def tearDown(self):
+        self.driver.close()
 
 if __name__ == "__main__":
     unittest.main()
