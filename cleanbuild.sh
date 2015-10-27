87ef692db5c2dbf8b1081f1d96a2331b9e7c700d
johanngomes <johanngomes@gmail.com>
2015-10-27 14:37:01 -0300

    changed cleanbuild.sh

--- cleanbuild.sh
+++ cleanbuild.sh
@@ -4,7 +4,8 @@ python3 -m unittest discover -s test.unit -p "*Test.py"
 
 echo "Running functional tests...\n\n"
 
+python3 manage.py runserver &
+sleep 5
+
 cd test/functional
 for f in *.py; do python3 "$f"; done
-
-echo "DONE! BUILD OK! :)"
