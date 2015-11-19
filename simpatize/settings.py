497e9903311822d9c7b280994bcdf2d015ba66fe
johanngomes <johanngomes@gmail.com>
2015-11-18 21:32:56 -0300

    Logging configuration added.

--- simpatize/settings.py
+++ simpatize/settings.py
@@ -131,3 +131,38 @@ STATICFILES_DIRS = (
 # Google Places API key
 
 GOOGLE_PLACES_API_KEY = "AIzaSyAfZcyrKMHxsidSpMsUcwLlOWbLsVZS0W4"
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'MYAPP': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
