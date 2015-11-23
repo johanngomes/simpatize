4558826a3ef650925e271ce5d65ba73212649feb
johanngomes <johanngomes@gmail.com>
2015-11-23 16:53:49 -0300

    Refactoring - [Marcela & Johann] - Transformed the system into a web service RESTful.

--- simpatize/views.py
+++ simpatize/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-
-from src.PlaceHelper import PlaceHelper
+from django.http import JsonResponse
 from src.RequestsGooglePlaces import RequestsGooglePlaces
 
 
 def index(request):
-    return render(request, "templates/index.html")
+    return render(request, "templates/info.html")
 
 
 def search(request):
@@ -16,8 +15,4 @@ def search(request):
 
     request_result = RequestsGooglePlaces.request_recife_metropolitan_area_places(place_name, place_type)
 
-    PlaceHelper.extract_places(request_result["json"])
-
-    return render(request, "templates/search.html",
-                  {"places": PlaceHelper.places,
-                   "warning_message": request_result["warning_message"]})
+    return JsonResponse(request_result["json"])
--- test/js/spec/geolocationSpec.js
+++ test/js/spec/geolocationSpec.js
@@ -1,5 +1,7 @@
 describe("geolocation", function() {
-    it("should return geolocation if 'nearby_places_yes' is checked", function() {
+    it("test", function() {
+        var dummyElement = document.createElement('input');
+
 
         };
     });
